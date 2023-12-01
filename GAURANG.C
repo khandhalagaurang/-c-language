03dd0de5028c1fcd0b350cf2592a7da28cf2ed26
Khandhala Gaurang <149217937+khandhalagaurang@users.noreply.github.com>
2023-12-01 11:59:10 +0530

    Add files via upload

---
+++ GAURANG.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+  clrscr();
+  printf("Name: gaurang khandhala\n");
+  printf("Email ID:f452gaurangkhnadhala@email.com\n");
+  printf("Mobile No: 9974565947\n");
+  printf("Date of Birth: 27 june 2005\n");
+  printf("Education: B.Com\n");
+  printf("GR ID: 6309\n");
+  printf("Add: plot vistar, vasna , dhandhuka , ahmedabad-382460");
+  getch();
+}
