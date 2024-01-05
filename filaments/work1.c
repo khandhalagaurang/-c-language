e8000dab8787ef3221620aa72eb98c83d660ab54
Khandhala Gaurang <149217937+khandhalagaurang@users.noreply.github.com>
2024-01-05 12:20:43 +0530

    Update work1.c

--- filaments/work1.c
+++ filaments/work1.c
@@ -2,24 +2,24 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-     char str[20];
-     int i, len, flag = 0;
+     char str[20],str2[20];
+     int ans;
      printf("enetr name:");
      gets(str);
-     printf("name is:");
-     puts(str);
-      len = strlen(str);
-
-    for (i = 0; i < len; i++) {
-        if (str[i] != str[len - i - 1]) {
-            flag = 1;
-            break;
-        }
-    }
-    if (flag){
-        printf("%s is not palindrome", str);
-    } else{
-        printf("%s is palindrome", str);
-    }      
+     strcpy(str2,str);
+     printf("\n name is %s",str);
+     printf("\n name2 is %s",str2);
+     strrev(str);
+     printf("\n reverse name is %s",str);
+     ans=strcmp(str,str2);
+     if (ans==0)
+     {
+        printf("\n it is palingdrome");
+     }else
+     {
+        printf("\n it is not palindrome");
+     }
+     
+     
     return 0;
 }
