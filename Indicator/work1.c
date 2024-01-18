7753bcdb320854ff5637fd0342052f3efb2ebd08
Khandhala Gaurang <149217937+khandhalagaurang@users.noreply.github.com>
2024-01-18 10:46:35 +0530

    Update work1.c

--- Indicator/work1.c
+++ Indicator/work1.c
@@ -1,23 +1,24 @@
 // swap two numbers using pointers
 #include<stdio.h>                  
-void swap(int *x , int *y);
 void main(){
-    int a,b;
+    int a,b,c;
+    int *ptr1, *ptr2;
+
     printf("enter two num ");
     scanf("%d %d",&a,&b);
 
     printf("\n a is  %d",a);
     printf("\n b is  %d \n",b);
 
-    swap(&a,&b);
+    ptr1 = &a;
+    ptr2 = &b;
+
+     c = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = c ;
+
     
     printf("\n after swaping");
     printf("\n a is = %d",a);
     printf("\n b is = %d",b);
-}
-void swap(int *x , int *y){
-    int z;
-    z=*x;
-    *x=*y;
-    *y=z;
 }
